991e1e97db358f3e73034a2d64a6a3f4885f9aa5
Ustun Ertas <ustunertas0@gmail.com>
2022-04-02 23:50:56 +0200

    ustun

--- src/Projects_1/reverseArr.java
+++ src/Projects_1/reverseArr.java
@@ -1,4 +1,7 @@
 package Projects_1;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 public class reverseArr {
     /*
@@ -9,7 +12,6 @@ public class reverseArr {
         Use Arrays.toString(your_array_name);
      */
 
-
     /*
           Verilen String array
 tring arrayi ters çevir
@@ -18,20 +20,19 @@ ters arrayi yazdır
        Arrays.toString(your_array_name);  <--- kullanın
      */
 
-
+    //Birinci yol
     public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-
-        String myStr = scan.nextLine();
-
-        String[] useThisArray = myStr.split(" ");
-
-        //        code start here
-        //        Koda burdan başlayın
-
-
-
+        int[] arr ={1,2,3,4,5,6};// int degerinde array dizisini olusturdum
+        int tersArr[]=new int[arr.length];// array dizilerin uzunluguna olasmak icin
+        int x=0;//  x adinda bir variable olusturdum sifira esitledim
+        int y=arr.length-1;
+        //  arrayiTersineCevir(x,y,arr,tersArr);// method olusturmak icin yaptim
+        System.out.println(Arrays.toString(tersArr));// arrayimi tersini yazdirmak icin
     }
+    // private static int[] arrayiTersineCevir(int x, int y, int[] arr, int[] tersArr) {
+    // if (Devami Gelecel){
 
+
+    //   }
+    // }
 }
